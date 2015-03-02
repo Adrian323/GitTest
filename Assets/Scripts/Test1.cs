16b844bca000da66db8e5c3a3ec325ac56392761
Adrian323 <andy.tache@gmail.com>
2015-03-02 17:02:52 +0200

    Commit

---
+++ Assets/Scripts/Test1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test1 : MonoBehaviour 
+{
+    public AudioClip[] ClipTest;
+    public Renderer[] Renderers;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
